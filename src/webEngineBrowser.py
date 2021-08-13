8d861e9210e95bf0ea93aa7275dfed15d9fbc115
Dylan <geradylanalvandi@gmail.com>
2021-08-13 05:39:23 +0200

    commented webBrowserTest

--- src/webEngineBrowser.py
+++ src/webEngineBrowser.py
@@ -2,24 +2,23 @@ import customStyleSheet as cs
 from urllib.parse import urlparse
 from PyQt5.QtCore import QUrl
 from PyQt5.QtWebEngineWidgets import QWebEngineView
-from PyQt5.QtWidgets import QFrame, QGridLayout, QHBoxLayout, QLineEdit, QPushButton, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QFrame, QHBoxLayout, QLineEdit, QPushButton, QVBoxLayout, QWidget
  
 class Browser(QWidget):
     def __init__(self, size=[800,800], default_url='https://www.google.com'):
+        #init and frame setup
         super().__init__()
         self.frame = QFrame(self)
         self.frame.resize(size[0],size[1])
         self.frame.setStyleSheet(cs.webBrowser_style)
         self.resize(size[0],size[1])
 
-        self.vertLayout = QVBoxLayout(self.frame)
-        self.vertLayout.setContentsMargins(0,0,0,0)
-        self.vertLayout.setSpacing(0)
-        self.horizontalLayout = QHBoxLayout()
-
+        #Setup WebEngine WebView
         self.web = QWebEngineView()
         self.web.load(QUrl(default_url))
 
+        #TOOLBAR
+        self.horizontalLayout = QHBoxLayout()
         self.url_line = QLineEdit(self.frame)
         self.back_button = QPushButton('<<', self.frame)
         self.forward_button = QPushButton('>>', self.frame)
@@ -27,7 +26,17 @@ class Browser(QWidget):
         self.close_button = QPushButton('X', self.frame)
         [x.setStyleSheet(cs.small_icon_style) for x in [self.back_button, self.forward_button, self.min_button, self.close_button]]
         self.url_line.setStyleSheet(cs.small_line_style)
+        
+        #Connect toolbar items
+        self.back_button.clicked.connect(self.web.back)
+        self.forward_button.clicked.connect(self.web.forward)
+        self.min_button.clicked.connect(lambda x: self.setVisible(x), False)
+        self.close_button.clicked.connect(self.webclose)
+        self.url_line.returnPressed.connect(self.browse)
+        self.url_line.setText(default_url)
+        self.web.urlChanged.connect(lambda url: self.url_line.setText(url.toString()))
 
+        #Add toolbars item to hlayout
         self.horizontalLayout.addWidget(self.back_button, 1)
         self.horizontalLayout.addWidget(self.forward_button, 1)
         self.horizontalLayout.addWidget(self.url_line, 8)
@@ -41,19 +50,15 @@ class Browser(QWidget):
         [self.quickAccessLayout.addWidget(btn) for btn in self.fav_buttons]
         [btn.setStyleSheet(cs.small_icon_style) for btn in self.fav_buttons]
 
+        #Add everything to one layout
+        self.vertLayout = QVBoxLayout(self.frame)
+        self.vertLayout.setContentsMargins(0,0,0,0)
+        self.vertLayout.setSpacing(0)
         self.vertLayout.addLayout(self.horizontalLayout)
         self.vertLayout.addWidget(self.web)
         self.vertLayout.addLayout(self.quickAccessLayout)
-        
 
-        self.back_button.clicked.connect(self.web.back)
-        self.forward_button.clicked.connect(self.web.forward)
-        self.min_button.clicked.connect(lambda x: self.setVisible(x), False)
-        self.close_button.clicked.connect(self.webclose)
-        self.url_line.returnPressed.connect(self.browse)
-        self.url_line.setText(default_url)
-        self.web.urlChanged.connect(lambda url: self.url_line.setText(url.toString()))
-
+    #browse the given url both from url_line in toolbar and from QuickAccess Toolbar
     def browse(self, fav=''):
         if fav:
             self.url_line.setText(fav)
@@ -71,6 +76,7 @@ class Browser(QWidget):
             if parsedUrl:
                 self.web.load(QUrl(parsedUrl))
 
+    #close and set enabled to false (for if in main)
     def webclose(self):
         self.web.close()
         self.setEnabled(False)
